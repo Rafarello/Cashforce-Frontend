d545cd0a97a40afbb2080249c9232f0b0e946208
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2022-12-29 19:23:45 -0300

    Feat: Criando estrutura de App inicial

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ContentSection from "./components/content/ContentSection.vue";
+import NavComponent from "./components/nav/NavComponent.vue";
+</script>
+
+<template>
+  <NavComponent />
+  <ContentSection />
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  background-color: #fbfdfe;
+}
+</style>
