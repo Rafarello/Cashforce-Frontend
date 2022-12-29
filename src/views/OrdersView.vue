e5eb880198634cd156d008de382c9577347b22ad
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2022-12-29 19:35:33 -0300

    Feat: View de Orders - inicial

---
+++ src/views/OrdersView.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <section className="orders-content">orders section</section>
+</template>
+
+<style scoped>
+.orders-content {
+  padding-left: 48px;
+  padding-top: 40px;
+  padding-right: 47px;
+}
+</style>
