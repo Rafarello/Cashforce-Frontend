654d1eec91ef41f0c2be037c14d43ae3743560a0
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2022-12-29 20:03:57 -0300

    Feat: Renderizando RouterView em ContentSection

--- src/components/content/ContentSection.vue
+++ src/components/content/ContentSection.vue
@@ -1,9 +1,19 @@
 <script setup>
 import HeaderCashforce from "../header/HeaderCashforce.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <HeaderCashforce />
+  <section className="main">
+    <HeaderCashforce />
+    <RouterView />
+  </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  background-color: white;
+  width: 100%;
+  height: 100vh;
+}
+</style>
