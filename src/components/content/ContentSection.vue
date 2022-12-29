84928ba89a112df3744a76f3f01b95f8f6590ccd
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2022-12-29 19:22:23 -0300

    Feat: Importando HeaderCashforce em ContentSection

--- src/components/content/ContentSection.vue
+++ src/components/content/ContentSection.vue
@@ -1,10 +1,9 @@
 <script setup>
+import HeaderCashforce from "../header/HeaderCashforce.vue";
 </script>
 
 <template>
-
+  <HeaderCashforce />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
