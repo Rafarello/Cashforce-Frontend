af338a46b484388e26b5135ff6ca045dd8fea704
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2022-12-29 19:56:26 -0300

    Feat: Renderizando Title de Orders

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+import HandShakeSvg from "../components/icons/HandShakeSvg.vue";
+</script>
 
 <template>
-  <section className="orders-content">orders section</section>
+  <section className="orders-content">
+    <div className="orders-title-container">
+      <HandShakeSvg />
+      <span>Notas fiscais</span>
+    </div>
+  </section>
 </template>
 
 <style scoped>
@@ -9,5 +16,20 @@
   padding-left: 48px;
   padding-top: 40px;
   padding-right: 47px;
+}
+
+.orders-title-container {
+  display: flex;
+  align-items: center;
+  column-gap: 9px;
+}
+
+.orders-title-container > span {
+  font-family: "DM Sans";
+  color: #021b51;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 28px;
 }
 </style>
