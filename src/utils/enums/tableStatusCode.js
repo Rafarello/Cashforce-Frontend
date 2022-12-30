d2c6e8055a07f31f4358b4a07e248503731520c4
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2022-12-30 09:35:12 -0300

    Feat: Objeto com código de status e seus significados

---
+++ src/utils/enums/tableStatusCode.js
@@ -0,0 +1,13 @@
+const tableStatusCode = {
+  0: "Pendente de confirmação",
+  1: "Pedido confirmado",
+  2: "Não reconhece o pedido",
+  3: "Mercadoria não recebida",
+  4: "Recebida com avaria",
+  5: "Devolvida",
+  6: "Recebida com devolução parcial",
+  7: "Recebida e confirmada",
+  8: "Pagamento Autorizado",
+};
+
+export default tableStatusCode;
